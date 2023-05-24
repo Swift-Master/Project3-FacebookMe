6ab1631c2a7eff19353cc73b05c512369024692e
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-05-24 22:20:59 +0900

    [20230524] NavigationBar 적용

--- FaceBookProfile/FaceBookProfile/ViewController.swift
+++ FaceBookProfile/FaceBookProfile/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
+    }
+    
+    func setup() {
+        navigationController?.navigationBar.backgroundColor = UIColor(red: 0.361, green: 0.447, blue: 0.627, alpha: 1.000)
     }
 
 
